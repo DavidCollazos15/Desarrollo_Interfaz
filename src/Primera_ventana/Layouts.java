861691b8b65ed8edcccf255f490069e2fd3b8180
DavidGonzálezC <davidgonzalezcgm@gmail.com>
2023-09-26 10:19:32 +0200

    Si

---
+++ src/Primera_ventana/Layouts.java
@@ -0,0 +1,51 @@
+package Primera_ventana;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Layouts {
+    public static void main(String[] args) {
+        // Crear la ventana principal
+        JFrame ventana = new JFrame();
+        ventana.setBounds(10, 10, 200, 300);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Crear paneles
+        JPanel Panel1 = new JPanel();
+        JPanel Panel2 = new JPanel();
+        JPanel Panel3 = new JPanel();
+
+        //Botones
+        JButton boton01 = new JButton("Boton 01");
+        JButton boton02 = new JButton("Boton 02");
+        JButton boton03 = new JButton("Boton 03");
+
+        Panel1.add(boton01);
+        Panel2.add(boton02);
+        Panel3.add(boton03);
+
+        ventana.add(Panel1);
+        ventana.add(Panel2);
+
+        //Colores
+
+        ventana.setBackground(Color.GREEN);
+        Panel1.setBackground(Color.RED);
+        Panel2.setBackground(Color.BLUE);
+        Panel3.setBackground(Color.MAGENTA);
+
+        //Layouts
+
+        ventana.setLayout(new FlowLayout());
+        //ventana.setLayout(new BorderLayout());
+
+        /* Agregar los paneles a la ventana en regiones específicas (BorderLayout)
+
+        ventana.add(Panel1, BorderLayout.NORTH);
+        ventana.add(Panel2, BorderLayout.SOUTH);
+        ventana.add(Panel3, BorderLayout.CENTER); */
+
+
+        ventana.setVisible(true);
+    }
+}
---
+++ src/Primera_ventana/Ventana.java
@@ -0,0 +1,101 @@
+package Primera_ventana;
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana {
+
+    public static void main(String[] args) {
+
+        // Crear la ventana principal
+        JFrame miPrimeraVentana = new JFrame();
+        miPrimeraVentana.setBounds(10, 10, 200, 300);
+        miPrimeraVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Crear paneles
+        JPanel PanelPrincipal = new JPanel(new GridLayout(7, 1, 0, 10)); // 6 filas, 1 columna, espaciado vertical de 10
+        JPanel Panel1 = new JPanel();
+        JPanel Panel2 = new JPanel();
+        JPanel Panel3 = new JPanel();
+        JPanel Panel4 = new JPanel();
+        JPanel Panel5 = new JPanel();
+        JPanel Panel6 = new JPanel();
+
+
+        // Etiqueta apellidos
+        JLabel apellidos = new JLabel("Apellidos:");
+        Panel1.add(apellidos);
+
+        // Crear el JTextField
+        JTextField apellidosField = new JTextField(20); // 20 es el ancho inicial del campo de texto
+        Panel1.add(apellidosField);
+
+        // Etiqueta nombre
+        JLabel nombre = new JLabel("Nombre:");
+        Panel2.add(nombre);
+
+        // Crear el JTextField
+        JTextField NombreField = new JTextField(20);
+        Panel2.add(NombreField);
+
+        // Etiqueta sexo
+        JLabel sexo = new JLabel("Sexo:");
+        Panel3.add(sexo);
+
+        // Crear un grupo para los JCheckBox de sexo
+        ButtonGroup grupoSexo = new ButtonGroup();
+
+        // Crear JCheckBox para "Masculino"
+        JCheckBox masculinoCheckBox = new JCheckBox("Masculino");
+        grupoSexo.add(masculinoCheckBox);
+        Panel3.add(masculinoCheckBox);
+
+        // Crear JCheckBox para "Femenino"
+        JCheckBox femeninoCheckBox = new JCheckBox("Femenino");
+        grupoSexo.add(femeninoCheckBox);
+        Panel3.add(femeninoCheckBox);
+
+        // Etiqueta nacionalidad
+        JLabel nacionalidad = new JLabel("Nacionalidad:");
+        Panel4.add(nacionalidad);
+
+        // Crear ComboBox nacionalidad
+        JComboBox NacionalidadBox = new JComboBox<>();
+        NacionalidadBox.addItem("");
+        NacionalidadBox.addItem("España");
+        NacionalidadBox.addItem("Francia");
+        NacionalidadBox.addItem("Alemania");
+        NacionalidadBox.addItem("Italia");
+        Panel4.add(NacionalidadBox);
+
+        // Etiqueta nacimiento
+        JLabel nacimiento = new JLabel("Fecha de nacimiento:");
+        Panel5.add(nacimiento);
+
+        // Crear el JTextField
+        JTextField NacimientoField = new JTextField(20);
+        Panel5.add(NacimientoField);
+
+        // Etiqueta domicilio
+        JLabel domicilio = new JLabel("Domicilio:");
+        Panel6.add(domicilio);
+
+        // Crear el JTextField
+        JTextField DomicilioField = new JTextField(20);
+        Panel6.add(DomicilioField);
+
+        //Añadirlos al PanelPrincipal
+
+        PanelPrincipal.add(Panel1);
+        PanelPrincipal.add(Panel2);
+        PanelPrincipal.add(Panel3);
+        PanelPrincipal.add(Panel4);
+        PanelPrincipal.add(Panel5);
+        PanelPrincipal.add(Panel6);
+
+        //Añadirlos a la ventana y hacerla visibel
+
+        miPrimeraVentana.add(PanelPrincipal);
+        miPrimeraVentana.setVisible(true);
+    }
+
+}
