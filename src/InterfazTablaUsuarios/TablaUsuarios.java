12bb178000f1230d9c561e271695d02254db91e8
DavidGonzálezC <davidgonzalezcgm@gmail.com>
2023-11-02 10:35:59 +0100

    Desarrollo

--- src/InterfazTablaUsuarios/TablaUsuarios.java
+++ src/InterfazTablaUsuarios/TablaUsuarios.java
@@ -1,6 +1,9 @@
 package InterfazTablaUsuarios;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,58 +11,100 @@ import java.awt.event.ActionListener;
 
 public class TablaUsuarios {
     public static void main(String[] args) {
-
         JFrame ventana = new JFrame();
-        ventana.setBounds(10, 10, 600, 400);
+        ventana.setBounds(10, 10, 700, 400);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.getContentPane().setLayout(new BorderLayout(0, 0));
 
-        //Paneles
-        JPanel inputPanel = new JPanel();
-        ventana.getContentPane().add(inputPanel, BorderLayout.NORTH);
+        // Establece el tamaño mínimo de la ventana
+        ventana.setMinimumSize(new Dimension(700, 400));
 
-        //Descripcion
-        JLabel nombreLabel = new JLabel("Nombre: ");
-        JLabel apellidosLabel = new JLabel("Apellidos: ");
+        // Crear el contenedor principal con GridBagLayout
+        JPanel container = new JPanel(new GridBagLayout());
+        ventana.add(container);
+
+        // Paneles
+        JPanel TituloPanel = new JPanel();
+        JPanel inputPanel = new JPanel(new GridBagLayout()); // Panel para introducción de datos
+        JPanel buttonPanel = new JPanel(); // Panel para los botones
+        GridBagConstraints ordenar = new GridBagConstraints(); // Crear el GridBagLayout
+
+        // Título
+        JLabel tituloLabel = new JLabel("Gestión de Usuarios");
+        tituloLabel.setFont(new Font("Arial", Font.BOLD, 24)); // Ajustar el estilo del título
+
+        // Etiquetas y campos de la izquierda
+        JLabel idLabel = new JLabel("ID: ");
         JLabel dniLabel = new JLabel("DNI: ");
-        JLabel emailLabel = new JLabel("Email: ");
         JLabel passwordLabel = new JLabel("Password: ");
 
-        //Zona de texto
-        JTextField nombreField = new JTextField(20);
-        JTextField apellidosField = new JTextField(20);
-        JTextField dniField = new JTextField(20);
-        JTextField emailField = new JTextField(20);
-        JTextField passwordField = new JPasswordField(20);
+        JTextField idField = new JTextField(5);
+        idField.setEditable(false);
+        idField.setText("1"); // Establecer el ID inicial
 
-        //Botones
+        JTextField dniField = new JTextField(10);
+        JTextField passwordField = new JPasswordField(10);
+
+        // Etiquetas y campos de la derecha
+        JLabel nombreLabel = new JLabel("Nombre: ");
+        JLabel apellidosLabel = new JLabel("Apellidos: ");
+        JLabel emailLabel = new JLabel("Email: ");
+
+        JTextField nombreField = new JTextField(15);
+        JTextField apellidosField = new JTextField(20);
+        JTextField emailField = new JTextField(20);
+
+        // Botones
         JButton nuevoButton = new JButton("Nuevo");
         JButton anadirButton = new JButton("Añadir");
         JButton modificarButton = new JButton("Modificar");
         JButton eliminarButton = new JButton("Eliminar");
 
-        //Añadir a paneles
-        inputPanel.add(nombreLabel);
-        inputPanel.add(nombreField);
+        // Agregar componentes a los paneles
+        TituloPanel.add(tituloLabel);
 
-        inputPanel.add(apellidosLabel);
-        inputPanel.add(apellidosField);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(5, 5, 5, 5);
 
-        inputPanel.add(dniLabel);
-        inputPanel.add(dniField);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        inputPanel.add(idLabel, gbc);
+        gbc.gridy = 1;
+        inputPanel.add(dniLabel, gbc);
+        gbc.gridy = 2;
+        inputPanel.add(passwordLabel, gbc);
 
-        inputPanel.add(emailLabel);
-        inputPanel.add(emailField);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        inputPanel.add(idField, gbc);
+        gbc.gridy = 1;
+        inputPanel.add(dniField, gbc);
+        gbc.gridy = 2;
+        inputPanel.add(passwordField, gbc);
 
-        inputPanel.add(passwordLabel);
-        inputPanel.add(passwordField);
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        inputPanel.add(nombreLabel, gbc);
+        gbc.gridy = 1;
+        inputPanel.add(apellidosLabel, gbc);
+        gbc.gridy = 2;
+        inputPanel.add(emailLabel, gbc);
 
-        inputPanel.add(nuevoButton);
-        inputPanel.add(anadirButton);
-        inputPanel.add(modificarButton);
-        inputPanel.add(eliminarButton);
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        inputPanel.add(nombreField, gbc);
+        gbc.gridy = 1;
+        inputPanel.add(apellidosField, gbc);
+        gbc.gridy = 2;
+        inputPanel.add(emailField, gbc);
 
-        //Tabla
+        buttonPanel.add(nuevoButton);
+        buttonPanel.add(anadirButton);
+        buttonPanel.add(modificarButton);
+        buttonPanel.add(eliminarButton);
+
+        // Tabla
         DefaultTableModel ModeloTabla = new DefaultTableModel();
         ModeloTabla.addColumn("ID");
         ModeloTabla.addColumn("Nombre");
@@ -73,9 +118,27 @@ public class TablaUsuarios {
             Class<?> col_class = tabla.getColumnClass(column);
             tabla.setDefaultEditor(col_class, null);
         }
+
+        // Agregar un ListSelectionListener a la tabla para llenar los campos al seleccionar una fila
+        tabla.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                int selectedRow = tabla.getSelectedRow();
+                if (selectedRow != -1) {
+                    idField.setText(tabla.getValueAt(selectedRow, 0).toString());
+                    nombreField.setText(tabla.getValueAt(selectedRow, 1).toString());
+                    apellidosField.setText(tabla.getValueAt(selectedRow, 2).toString());
+                    dniField.setText(tabla.getValueAt(selectedRow, 3).toString());
+                    emailField.setText(tabla.getValueAt(selectedRow, 4).toString());
+                    passwordField.setText(tabla.getValueAt(selectedRow, 5).toString());
+                }
+            }
+        });
+
         JScrollPane scrollPane = new JScrollPane(tabla);
 
-        //Funciones de los botones
+        // Funciones de los botones
+        final int[] proximoID = {1}; // Variable para llevar el control del ID
         nuevoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -84,6 +147,7 @@ public class TablaUsuarios {
                 dniField.setText("");
                 emailField.setText("");
                 passwordField.setText("");
+                idField.setText(String.valueOf(proximoID[0]));
             }
         });
 
@@ -94,9 +158,12 @@ public class TablaUsuarios {
                 String apellidos = apellidosField.getText();
                 String dni = dniField.getText();
                 String email = emailField.getText();
+                String password = passwordField.getText();
 
                 if (!nombre.isEmpty() && !apellidos.isEmpty() && !email.isEmpty()) {
-                    ModeloTabla.addRow(new Object[]{ModeloTabla.getRowCount() + 1, nombre, apellidos, dni, email});
+                    ModeloTabla.addRow(new Object[]{proximoID[0], nombre, apellidos, dni, email, password});
+                    proximoID[0]++;
+                    idField.setText(String.valueOf(proximoID[0]));
                 } else {
                     JOptionPane.showMessageDialog(ventana, "Nombre, apellidos y email son campos obligatorios.");
                 }
@@ -112,12 +179,14 @@ public class TablaUsuarios {
                     String apellidos = apellidosField.getText();
                     String dni = dniField.getText();
                     String email = emailField.getText();
+                    String password = passwordField.getText();
 
                     if (!nombre.isEmpty() && !apellidos.isEmpty() && !email.isEmpty()) {
                         ModeloTabla.setValueAt(nombre, selectedRow, 1);
                         ModeloTabla.setValueAt(apellidos, selectedRow, 2);
                         ModeloTabla.setValueAt(dni, selectedRow, 3);
                         ModeloTabla.setValueAt(email, selectedRow, 4);
+                        ModeloTabla.setValueAt(password, selectedRow, 5);
                     } else {
                         JOptionPane.showMessageDialog(ventana, "Nombre, apellidos y email son campos obligatorios.");
                     }
@@ -142,7 +211,28 @@ public class TablaUsuarios {
             }
         });
 
-        ventana.getContentPane().add(scrollPane, BorderLayout.CENTER);
+        // Agregar componentes al contenedor utilizando GridBagLayout
+        ordenar.fill = GridBagConstraints.BOTH;
+        ordenar.weightx = 1.0;
+        ordenar.weighty = 0.0;
+        ordenar.gridwidth = 1;
+
+        ordenar.gridy = 0;
+        container.add(TituloPanel, ordenar);
+
+        ordenar.gridx = 0;
+        ordenar.gridy = 1;
+        container.add(inputPanel, ordenar);
+
+        ordenar.gridy = 2;
+        container.add(buttonPanel, ordenar);
+
+        ordenar.weighty = 1.0;
+        ordenar.gridy = 3;
+        container.add(scrollPane, ordenar);
+
+        ventana.add(container);
         ventana.setVisible(true);
     }
 }
+
